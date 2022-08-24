9abbb9b0f554e1e2bdf787c12b5e7ce676a85a8f
safin saber <safinsabir@gmail.com>
2022-08-24 22:13:42 +0300

    authentication Done

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tutor_group/app%20bar%20and%20drawers/my_profile_appbar.dart';
@@ -20,6 +21,8 @@ class _ProfileState extends State<Profile> {
         : utils.textBlackL;
     final bool isAndroid = GetPlatform.isAndroid;
     bool isDark = Theme.of(context).brightness == Brightness.dark;
+    dynamic data = FirebaseFirestore.instance.collection("Teachers").get();
+    // UserModel user = UserModel.fromSnapShot(data);
     return SafeArea(
       child: Scaffold(
         appBar: myHomeAppBar(
@@ -32,7 +35,7 @@ class _ProfileState extends State<Profile> {
                 splashColor: Colors.transparent,
                 onPressed: () {
                   Get.to(
-                    () => const Settings(),
+                    const Settingsr(),
                     transition: Transition.cupertino,
                   );
                 },
@@ -46,39 +49,35 @@ class _ProfileState extends State<Profile> {
         body: SizedBox(
           width: double.infinity,
           height: double.infinity,
-          child: Stack(
-            children: const [
-              // Positioned(
-              //   top: isAndroid ? 5 : 10,
-              //   left: 20,
-              //   right: 20,
-              //   child: Row(
-              //     crossAxisAlignment: CrossAxisAlignment.start,
-              //     children: [
-              //       const Text(
-              //         'Profile',
-              //         style:
-              //             TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              //       ),
-              //       const Spacer(),
-              //       IconButton(
-              //         highlightColor: Colors.transparent,
-              //         splashColor: Colors.transparent,
-              //         onPressed: () {
-              //           Get.to(
-              //             () => const Settings(),
-              //             transition: Transition.cupertino,
-              //           );
-              //         },
-              //         icon: Icon(
-              //           Icons.settings,
-              //           color: color,
-              //           size: 25,
-              //         ),
-              //       ),
-              //     ],
-              //   ),
-              // ),
+          child: Column(
+            children: [
+              Expanded(
+                child: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                  future: data,
+                  builder: ((context, snapshot) {
+                    // UserModel userModel =
+                    //     UserModel.fromMap(snapshot.data!.data()!);
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator.adaptive(),
+                      );
+                    }
+                    return Column(
+                      children: const [
+                        // SizedBox(
+                        //   height: 150,
+                        //   child: Image.network(
+                        //       snapshot.data!.data()!['imageUrl']),
+                        // // ),
+                        // Text(snapshot.data!.data()!['name'].toString()),
+                        // Text(snapshot.data!.data()!['surname'].toString()),
+                        // Text(snapshot.data!.data()!['email'].toString()),
+                        // Text(snapshot.data!.data()!['phoneNumber'].toString()),
+                      ],
+                    );
+                  }),
+                ),
+              )
             ],
           ),
         ),
@@ -86,3 +85,34 @@ class _ProfileState extends State<Profile> {
     );
   }
 }
+// Positioned(
+//   top: isAndroid ? 5 : 10,
+//   left: 20,
+//   right: 20,
+//   child: Row(
+//     crossAxisAlignment: CrossAxisAlignment.start,
+//     children: [
+//       const Text(
+//         'Profile',
+//         style:
+//             TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//       ),
+//       const Spacer(),
+//       IconButton(
+//         highlightColor: Colors.transparent,
+//         splashColor: Colors.transparent,
+//         onPressed: () {
+//           Get.to(
+//             () => const Settings(),
+//             transition: Transition.cupertino,
+//           );
+//         },
+//         icon: Icon(
+//           Icons.settings,
+//           color: color,
+//           size: 25,
+//         ),
+//       ),
+//     ],
+//   ),
+// ),
